8d089b161e4afa895e9a953cd9e8db88f4ce39ba
Dev Shah <dev.shah@specindia.com>
2026-07-21 13:02:47 +0530

    api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,7 @@
 // const BASE_URL = "http://127.0.0.1:8000"
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+// const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "https://pythonwork.onrender.com";
 console.log("API URL:", BASE_URL);
 export async function AddBooks(params) {
     console.log("ADD BOOK FUNCTION CALLED", bookData);
