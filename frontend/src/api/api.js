db5587b9513d5067f5b312c408e23ab72e46cf02
Dev Shah <dev.shah@specindia.com>
2026-07-21 13:14:28 +0530

    api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "http://127.0.0.1:8000"
 const BASE_URL = import.meta.env.VITE_API_URL;
-console.log("API URL:", BASE_URL);
+
 export async function AddBooks(params) {
   console.log("ADD BOOK FUNCTION CALLED", params);
     const response  = await fetch(`${BASE_URL}/add-books`,{
